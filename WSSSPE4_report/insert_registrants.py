48ebe4efbc3e37b56200316fdfe98057a3f5f983
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2016-09-14 14:21:35 +0100

    added registrants parsing script

---
+++ WSSSPE4_report/insert_registrants.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import re, operator
+
+with open("registrants.txt", 'r') as f:
+  people = []
+  person = {}
+  for line in f:
+    if 'institution: ' in line:
+      person['institution'] = re.search('institution: (.*)', line).group(1).replace('&', r'\&')
+    elif 'email: ' in line:
+      person['email'] = re.search('email: (.*)', line).group(1)
+    elif len(line) > 2:
+      person['name'] = line[:-1].replace('&', r'\&')
+    else:
+      people.append(person)
+      person = {}
+if 'name' in person:
+  people.append(person)
+
+for person in sorted(people, key=lambda x: x['name'].split()[-1]):
+  #print(person['name'] + ' & ' + person['institution'] + r'\\')
+  print(person['name'] + ' & ' +
+        r'\href{mailto:' + person['email'] + r'}{' + person['email'] + r'}&' +
+        person['institution'] + r'\\'
+       )
